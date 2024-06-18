b27d8e5209d27b3cfd590245b6fcd9ea7ff04af5
luigi sato <base.basketball0607@gmail.com>
2024-06-18 10:12:04 +0900

    cut out each process into methods

--- backend-project2/remote-procedure-call/server.py
+++ backend-project2/remote-procedure-call/server.py
@@ -26,37 +26,67 @@ functions = {
     'sort': sort
 }
 
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-server_address = './socket_file'
-
-try:
-    os.unlink(server_address)
-except FileNotFoundError:
-    pass
-
-sock.bind(server_address)
-
-sock.listen(1)
-
-while True:
-    connection, client_address = sock.accept()
+def connection():
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    server_address = './socket_file'
 
     try:
-        while True:
-            data = connection.recv(4096)
-            if data:
-                request = json.loads(data.decode('utf-8'))
+        os.unlink(server_address)
+    except FileNotFoundError:
+        pass
 
-                if request['method'] in functions:
-                    result = functions[request['method']](*request['params'])
-                    print(result)
-                    response = {"results": result, "result_type": type(result).__name__, "id": 1}
-                    response_json = json.dumps(response)
-                    connection.sendall(response_json.encode())
+    sock.bind(server_address)
+
+    sock.listen(1)
+
+    while True:
+        connection, client_address = sock.accept()
+
+        try:
+            while True:
+                data = connection.recv(4096)
+                if data:
+                    request = json.loads(data.decode('utf-8'))
+
+                    try:
+                        result = handle_request(request)
+                        response = create_response(result)
+                        connection.sendall(response.encode())
+                    except Exception as error:
+                        response = create_error_response(error)
+                        connection.sendall(response.encode())
                 else:
-                    connection.sendall('Method not found'.encode())
-            else:
-                break
+                    break
 
-    finally:
-        connection.close()
+        finally:
+            connection.close()
+
+def handle_request(request):
+    try:
+        result = functions[request['method']](*request['params'])
+        return result
+    except Exception as error:
+        raise error
+
+def create_response(result):
+    response = {
+        "results": result,
+        "result_type": type(result).__name__,
+        "id": 1
+    }
+    return json.dumps(response)
+
+def create_error_response(error):
+    response = {
+        "error": {
+            "message": str(error)
+        },
+        "id": 1
+    }
+    return json.dumps(response)
+
+def main():
+    connection()
+
+if __name__ == '__main__':
+    main()
